ec45c476c2c8a36ef249dc61b6cff83dac1b1adf
Oleksandr Semeniuta <oleksandr.semeniuta@gmail.com>
2019-02-24 09:19:32 +0100

    Parsing of JSON arrays in main; integrate readMap

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,8 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
+#include "init.h"
+#include "PathPlanner.h"
 #include "json.hpp"
 
 // for convenience
@@ -14,6 +16,7 @@ using std::string;
 using std::vector;
 
 int main() {
+
   uWS::Hub h;
 
   // Load up map values for waypoint's x,y,s and d normalized normal vectors
@@ -25,30 +28,13 @@ int main() {
 
   // Waypoint map to read from
   string map_file_ = "../data/highway_map.csv";
+
   // The max s value before wrapping around the track back to 0
   double max_s = 6945.554;
 
-  std::ifstream in_map_(map_file_.c_str(), std::ifstream::in);
+  readMap(map_file_, map_waypoints_x, map_waypoints_y, map_waypoints_s, map_waypoints_dx, map_waypoints_dy);
 
-  string line;
-  while (getline(in_map_, line)) {
-    std::istringstream iss(line);
-    double x;
-    double y;
-    float s;
-    float d_x;
-    float d_y;
-    iss >> x;
-    iss >> y;
-    iss >> s;
-    iss >> d_x;
-    iss >> d_y;
-    map_waypoints_x.push_back(x);
-    map_waypoints_y.push_back(y);
-    map_waypoints_s.push_back(s);
-    map_waypoints_dx.push_back(d_x);
-    map_waypoints_dy.push_back(d_y);
-  }
+  std::ifstream in_map_(map_file_.c_str(), std::ifstream::in);
 
   h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,
                &map_waypoints_dx,&map_waypoints_dy]
@@ -93,11 +79,59 @@ int main() {
           vector<double> next_x_vals;
           vector<double> next_y_vals;
 
+          // ===========================================================================
+
           /**
            * TODO: define a path made up of (x,y) points that the car will visit
            *   sequentially every .02 seconds
            */
 
+          // Parsing the sensor_fusion JSON
+
+          std::vector<std::vector<double>> sf_data;
+
+          for (auto it_vehicle = sensor_fusion.begin(); it_vehicle != sensor_fusion.end(); it_vehicle++) {
+
+            auto vehicle_json = *it_vehicle;
+
+            std::vector<double> vehicle;
+
+            for (auto it_num = vehicle_json.begin(); it_num != vehicle_json.end(); it_num++) {
+              vehicle.push_back(*it_num);
+            }
+
+            sf_data.push_back(vehicle);
+
+          }
+
+          // Parsing previous path (x, y) JSONs
+
+          std::vector<double> prev_x_data;
+          std::vector<double> prev_y_data;
+
+          for (auto it_num = previous_path_x.begin(); it_num != previous_path_x.end(); it_num++) {
+            prev_x_data.push_back(*it_num);
+          }
+
+          for (auto it_num = previous_path_y.begin(); it_num != previous_path_y.end(); it_num++) {
+            prev_y_data.push_back(*it_num);
+          }
+
+          PathPlanner::input in{
+            car_x,
+            car_y,
+            car_s,
+            car_d,
+            car_yaw,
+            car_speed,
+            prev_x_data,
+            prev_y_data,
+            end_path_s,
+            end_path_d,
+            sf_data
+          };
+
+          // ===========================================================================
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
